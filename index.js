36e7b023895ff429c8456d57577d9e2a91f15c30
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-06 22:57:16 +0400

    created a reducer function

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const orderCake = () => {
     quantity: 1,
   };
 };
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
