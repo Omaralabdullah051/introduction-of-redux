4c36a12797532259edb671c56f31260eb829d7a0
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-06 23:15:00 +0400

    created a store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 //*action creater- which returns an action
@@ -23,3 +26,15 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+const unSubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unSubscribe();
