32df1b56834e693874c087fa63fee4edee37bbbd
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-07 07:43:30 +0400

    handle multiple actions and update states by using a single reducer

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
 //*action creater- which returns an action
-const orderCake = () => {
+const orderCake = (qty = 1) => {
   return {
     type: CAKE_ORDERED,
-    payload: 1,
+    payload: qty,
   };
 };
 
@@ -20,8 +22,23 @@ const restockCake = (qty = 1) => {
   };
 };
 
+const orderIceCream = (qty = 1) => {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+};
+
+const restockIceCream = (qty = 1) => {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+};
+
 const initialState = {
   numOfCakes: 10,
+  numOfIceCreams: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -29,12 +46,22 @@ const reducer = (state = initialState, action) => {
     case CAKE_ORDERED:
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1,
+        numOfCakes: state.numOfCakes - action.payload,
       };
     case CAKE_RESTOCKED:
       return {
         ...state,
         numOfCakes: state.numOfCakes + action.payload,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
       };
     default:
       return state;
@@ -53,10 +80,16 @@ const unSubscribe = store.subscribe(() =>
 // store.dispatch(orderCake());
 // store.dispatch(restockCake(3));
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+
 actions.orderCake();
 actions.orderCake();
 actions.orderCake();
 actions.restockCake(3);
-
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(3);
 unSubscribe();
